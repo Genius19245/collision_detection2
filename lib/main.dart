6b5ce60fa0dd03d646009cc5c1a8ea290740a316
renukakelkar <renuvkelkar@gmail.com>
2022-10-31 22:13:14 +0000

    car collision

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,42 @@
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flame/input.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(GameWidget(game: MyGame()));
 }
 
-class MyGame extends FlameGame with HasCollisionDetection {
+class MyGame extends FlameGame with HasCollisionDetection, HasTappables {
   @override
   Future<void> onLoad() async {
     super.onLoad();
     add(SpriteComponent()
-      ..sprite = await loadSprite('background.jpeg')
+      ..sprite = await loadSprite('77.jpg')
       ..size = size);
-    add(Bowser(await loadSprite('bowser.png')));
-    add(Yoshi(await loadSprite('yoshi.png')));
+    add(Left(await loadSprite('bowser.png')));
+    add(Right(await loadSprite('bowser.png')));
+    add(Cars(await loadSprite('sports.png')));
+    add(Car(await loadSprite('sports.png')));
+    add(Player(await loadSprite('yoshis.png')));
   }
 }
 
-class Bowser extends SpriteComponent with CollisionCallbacks {
+class Car extends SpriteComponent with CollisionCallbacks {
   bool _collision = false;
-  Bowser(Sprite sprite) {
+  Car(Sprite sprite) {
+    flipHorizontally();
     this.sprite = sprite;
-    size = Vector2(75.0, 75.0);
+    size = Vector2(150.0, 100.0);
     anchor = Anchor.center;
-    position = Vector2(100.0, 200.0);
-    debugMode = true;
+    position = Vector2(1200.0, 550.0);
     add(RectangleHitbox());
   }
   @override
   void update(double dt) {
     super.update(dt);
-    x += 1;
+    x -= 4.5;
   }
 
   @override
@@ -40,19 +44,88 @@ class Bowser extends SpriteComponent with CollisionCallbacks {
       Set<Vector2> intersectionPoints, PositionComponent other) {
     super.onCollisionStart(intersectionPoints, other);
     _collision = true;
-    if (other is Yoshi) {
-      print('you lose try again');
+    if (other is Left) {
+      position = Vector2(1200.0, 550.0);
     }
   }
 }
 
-class Yoshi extends SpriteComponent with CollisionCallbacks {
-  Yoshi(Sprite sprite) {
+class Cars extends SpriteComponent with CollisionCallbacks {
+  bool _collision = false;
+  Cars(Sprite sprite) {
+    this.sprite = sprite;
+    size = Vector2(150.0, 100.0);
+    anchor = Anchor.center;
+    position = Vector2(100.0, 525.0);
+    debugMode = true;
+    add(RectangleHitbox());
+  }
+  @override
+  void update(double dt) {
+    super.update(dt);
+    x += 4.5;
+  }
+
+  @override
+  void onCollisionStart(
+      Set<Vector2> intersectionPoints, PositionComponent other) {
+    super.onCollisionStart(intersectionPoints, other);
+    _collision = true;
+    if (other is Right) {
+      position = Vector2(-300.0, 525.0);
+    }
+  }
+}
+
+class Left extends SpriteComponent with CollisionCallbacks {
+  Left(Sprite sprite) {
     this.sprite = sprite;
     size = Vector2(75.0, 75.0);
     anchor = Anchor.center;
-    position = Vector2(600.0, 200.0);
+    position = Vector2(-300.0, 500.0);
     debugMode = true;
     add(RectangleHitbox());
+  }
+}
+
+class Right extends SpriteComponent with CollisionCallbacks {
+  Right(Sprite sprite) {
+    this.sprite = sprite;
+    size = Vector2(75.0, 75.0);
+    anchor = Anchor.center;
+    position = Vector2(1400.0, 500.0);
+    debugMode = true;
+    add(RectangleHitbox());
+  }
+}
+
+class Player extends SpriteComponent with CollisionCallbacks, Tappable {
+  Player(Sprite sprite) {
+    this.sprite = sprite;
+    size = Vector2(65.0, 65.0);
+    anchor = Anchor.center;
+    position = Vector2(600.0, 650.0);
+    add(RectangleHitbox());
+  }
+
+  @override
+  bool onTapDown(TapDownInfo info) {
+    y -= 50;
+    x -= 50;
+    return super.onTapDown(info);
+  }
+
+  @override
+  void Update(dt) {
+    super.update(dt);
+  }
+
+  @override
+  void onCollisionStart(
+      Set<Vector2> intersectionPoints, PositionComponent other) {
+    super.onCollisionStart(intersectionPoints, other);
+    if (other is Cars) {
+      print('touching cars');
+    }
   }
 }
